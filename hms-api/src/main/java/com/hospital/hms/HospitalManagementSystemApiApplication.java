95c679baf2ca24f5d862beb38a4dd086c5acdb52
RedAvocado <minhcuong2922004@gmail.com>
2026-01-15 09:44:00 +0700

    set-up back-end and front-end

---
+++ hms-api/src/main/java/com/hospital/hms/HospitalManagementSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.hospital.hms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalManagementSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalManagementSystemApiApplication.class, args);
+	}
+
+}
