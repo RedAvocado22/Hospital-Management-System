a2535f46c3f1110f1b91aa5acdbe508a7f299469
longnh <nguyenhailong2708@gmail.com>
2026-02-26 18:19:55 +0700

    Thông đifea: add new view medicine detail service

---
+++ hms-api/src/main/java/com/hospital/hms/pharmacy/dto/response/MedicineResponse.java
@@ -0,0 +1,24 @@
+package com.hospital.hms.pharmacy.dto.response;
+
+
+import com.hospital.hms.base.response.BaseResponse;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.math.BigDecimal;
+
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class MedicineResponse extends BaseResponse {
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Integer quantity;
+    private Boolean isActive;
+}
