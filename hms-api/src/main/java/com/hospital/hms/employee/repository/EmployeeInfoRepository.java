757443bdf3f739d75f8742efd02c549916ee369d
RedAvocado <minhcuong2922004@gmail.com>
2026-03-08 15:17:58 +0700

    feat(employee): add detail view for employee

--- hms-api/src/main/java/com/hospital/hms/employee/repository/EmployeeInfoRepository.java
+++ hms-api/src/main/java/com/hospital/hms/employee/repository/EmployeeInfoRepository.java
@@ -4,8 +4,11 @@ import com.hospital.hms.employee.entity.EmployeeInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface EmployeeInfoRepository extends JpaRepository<EmployeeInfo, UUID>, JpaSpecificationExecutor<EmployeeInfo> {
     boolean existsByCode(String code);
+
+    Optional<EmployeeInfo> findById(UUID id);
 }
