dd3c8920bcadcc58959bf0b71918704df8f94795
RedAvocado <minhcuong2922004@gmail.com>
2026-03-09 08:19:39 +0700

    update: add validate size to input

--- hms-api/src/main/java/com/hospital/hms/auth/request/SignInRequest.java
+++ hms-api/src/main/java/com/hospital/hms/auth/request/SignInRequest.java
@@ -3,6 +3,7 @@ package com.hospital.hms.auth.request;
 import com.hospital.hms.base.request.BaseRequest;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -18,10 +19,12 @@ import lombok.experimental.SuperBuilder;
 public class SignInRequest extends BaseRequest {
 
     @NotBlank(message = "Username is required")
+    @Size(max = 50)
     @Schema(description = "Account username", example = "john.doe", requiredMode = Schema.RequiredMode.REQUIRED)
     private String username;
 
     @NotBlank(message = "Password is required")
+    @Size(max = 100)
     @Schema(description = "Account password", example = "Str0ng!Pass", requiredMode = Schema.RequiredMode.REQUIRED)
     private String password;
 }
