cf8feb3b13e2bc0d5b1c566be3850e1117e3021a
RedAvocado <minhcuong2922004@gmail.com>
2026-03-25 00:44:47 +0700

    update(medical-record): improve performance for medical record detail

--- hms-api/src/main/java/com/hospital/hms/medical/repository/MedicalRecordRepository.java
+++ hms-api/src/main/java/com/hospital/hms/medical/repository/MedicalRecordRepository.java
@@ -4,21 +4,22 @@ import com.hospital.hms.medical.entity.MedicalRecord;
 import com.hospital.hms.medical.response.MedicalRecordResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
-import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface MedicalRecordRepository extends JpaRepository<MedicalRecord, UUID> {
 
-    List<MedicalRecord> findByPatient_Id(UUID patientId);
-
-    List<MedicalRecord> findByDoctor_Id(UUID doctorId);
+    @Override
+    @EntityGraph(attributePaths = {"doctor", "patient", "patient.account"})
+    Optional<MedicalRecord> findById(UUID id);
 
     @Query(
             value = "SELECT BIN_TO_UUID(mr.id) as id, " +
